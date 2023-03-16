29df7a1520bbd18ee32c1ed7bc7c4552f3c1d2f5
gyojinK <gyojink@gyojinKui-MacBookAir.local>
2023-03-16 13:57:05 +0900

    개발환경 설정

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+
+console.log('Hello TypeScript');
+//transComfile : ts파일을 컴파일하면 자바스크립트 파일로 변형
+
+//터미널로 tsc --init 명령어를 이용하여 tsconfig.json 파일 생성
+//생성된 파일에서 소스코드의 경로와 컴파일된 자바스크립트 코드의 경로를 설정가능
+//  outDir : 컴파일된 코드(자바스크립트), rootDir : 컴파일전 tsc 소스코드
+
