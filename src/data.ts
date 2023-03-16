ecc675f5c305c4fa324b66606f8346b9cacd68ee
gyojinK <gyojink@gyojinKui-MacBookAir.local>
2023-03-16 15:22:12 +0900

    sample Data

---
+++ src/data.ts
@@ -0,0 +1,4 @@
+export const data = [  //타입추론(type inference)로 선언과 초기화를 동시에 진행하여 data배열은 객체형식으로 자동으로 형성
+    { id : 1, task : '프로세싱', complete : true},
+    { id : 2, task : 'TS 학습', complete : false},
+]
