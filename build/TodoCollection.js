8ab042acae80712cbf2bdf6eb9dce13c0d91e5f2
gyojinK <gyojink@gyojinKui-MacBookAir.local>
2023-03-16 16:13:38 +0900

    메소드 추가 & Map자료형 사용

--- build/TodoCollection.js
+++ build/TodoCollection.js
@@ -7,20 +7,33 @@ const TodoItem_1 = __importDefault(require("./TodoItem"));
 class TodoCollection {
     constructor(Username, todoItems = []) {
         this.Username = Username;
-        this.todoItems = todoItems;
         this.nextid = 1;
+        this.itemMap = new Map();
+        todoItems.forEach((item) => this.itemMap.set(item.id, item));
     }
     //item을 추가하는 메소드
     addTodo(task) {
         while (this.getTodoById(this.nextid)) {
             this.nextid++;
         }
-        this.todoItems.push(new TodoItem_1.default(this.nextid, task));
+        this.itemMap.set(this.nextid, new TodoItem_1.default(this.nextid, task));
         return this.nextid;
+    }
+    // includeComplete -> true : 모든 할일 목록 반환
+    // includeComplete -> false : 완료 목록은 제외한 할일 목록 반환
+    getTodoItems(includeComplete) {
+        return [...this.itemMap.values()].filter((item) => includeComplete || !item.complete); // ...은 전개연산자로 배열로 만들어줌
+    }
+    removeComplete() {
+        this.itemMap.forEach((item) => {
+            if (item.complete) {
+                this.itemMap.delete(item.id);
+            }
+        });
     }
     //item을 찾는 메소드
     getTodoById(id) {
-        return this.todoItems.find((item) => item.id === id);
+        return this.itemMap.get(id);
     }
     //item이 task를 완료했는지 상태를 변환해주는 메소드
     markComplete(id, complete) {
